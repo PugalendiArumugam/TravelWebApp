afd024c970bac226d28d75a23a223153d402f9cf
PugalendiArumugam <39829202+PugalendiArumugam@users.noreply.github.com>
2025-06-27 13:42:16 +0530

    nav bar update

---
+++ a_travel/lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class CustomColor {
+  static Color scaffoldBg = Color(0xff252734);
+  static Color bgLight1 = Color(0xff333646);
+  static Color bgLight2 = Color(0xff424657);
+  static Color textFieldBg = Color(0xffC8C9CE);
+  static Color hintDark = Color(0xff666874);
+  static Color yellowSecondary = Color(0xffFFC25C);
+  static Color yellowPrimary = Color(0xffFFAF29);
+  static Color whilePrimary = Color(0xffEAEAEB);
+  static Color whileSecondar = Color(0xffC8C9CE);
+}
--- a_travel/lib/pages/home_page.dart
+++ a_travel/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:a_travel/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,11 +12,33 @@ class _MyWidgetState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColor.scaffoldBg,
       body: ListView(
         scrollDirection: Axis.vertical,
         children: [
           // main
-          Container(height: 500, width: double.maxFinite),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.transparent, CustomColor.bgLight1],
+              ),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Row(
+              children: [
+                Text("Pugalendi"),
+                Spacer(),
+                for (int i = 0; i < 5; i++)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: TextButton(onPressed: () {}, child: Text("Button")),
+                  ),
+              ],
+            ),
+          ),
           // skills
           Container(
             height: 500,
